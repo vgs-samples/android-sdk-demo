e4fcd27a939f31c86e4e420acf217d81bc517172
Dmytro Kos <dmitriy.kosolapenko@gmail.com>
2020-01-20 18:37:08 +0200

    integrate VGSCollect

--- app/src/main/java/com/vgscollect/androiddemo/MainActivity.kt
+++ app/src/main/java/com/vgscollect/androiddemo/MainActivity.kt
@@ -2,11 +2,89 @@ package com.vgscollect.androiddemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.verygoodsecurity.vgscollect.core.HTTPMethod
+import com.verygoodsecurity.vgscollect.core.VGSCollect
+import com.verygoodsecurity.vgscollect.core.VgsCollectResponseListener
+import com.verygoodsecurity.vgscollect.core.model.VGSResponse
+import com.verygoodsecurity.vgscollect.core.model.state.FieldState
+import com.verygoodsecurity.vgscollect.core.storage.OnFieldStateChangeListener
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), VgsCollectResponseListener, View.OnClickListener {
+
+    private val vgsForm = VGSCollect("tntxrsfgxcn")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        submitBtn?.setOnClickListener(this)
+
+        vgsForm.onResponseListener = this
+
+        vgsForm.addOnFieldStateChangeListener(getOnFieldStateChangeListener())
+
+        vgsForm.bindView(cardNumberField)
+        vgsForm.bindView(cardCVCField)
+        vgsForm.bindView(cardHolderField)
+        vgsForm.bindView(cardExpDateField)
+    }
+
+    override fun onDestroy() {
+        vgsForm.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onClick(v: View?) {
+        progressBar?.visibility = View.VISIBLE
+        when (v?.id) {
+            R.id.submitBtn -> vgsForm.asyncSubmit(this@MainActivity, "/post", HTTPMethod.POST)
+        }
+    }
+
+    private fun getOnFieldStateChangeListener(): OnFieldStateChangeListener {
+        return object : OnFieldStateChangeListener {
+            override fun onStateChange(state: FieldState) {
+                titleHeader?.text = "STATE:"
+                val states = vgsForm.getAllStates()
+                val builder = StringBuilder()
+                states.forEach {
+                    builder.append(it.fieldName).append("\n")
+                        .append("   hasFocus: ").append(it.hasFocus).append("\n")
+                        .append("   isValid: ").append(it.isValid).append("\n")
+                        .append("   isEmpty: ").append(it.isEmpty).append("\n")
+                        .append("   isRequired: ").append(it.isRequired).append("\n")
+                    if (it is FieldState.CardNumberState) {
+                        builder.append("    type: ").append(it.cardBrand).append("\n")
+                            .append("       end: ").append(it.last).append("\n")
+                            .append("       bin: ").append(it.bin).append("\n")
+                            .append(it.number).append("\n")
+                    }
+
+                    builder.append("\n")
+                }
+                responseView?.text = builder.toString()
+            }
+        }
+    }
+
+    override fun onResponse(response: VGSResponse?) {
+        progressBar?.visibility = View.INVISIBLE
+
+        titleHeader?.text = "RESPONSE:"
+        when (response) {
+            is VGSResponse.SuccessResponse -> {
+                val builder = StringBuilder("CODE: ")
+                    .append(response.code.toString()).append("\n\n")
+                response.response?.forEach {
+                    builder.append(it.key).append(": ").append(it.value).append("\n\n")
+                }
+                responseView.text = builder.toString()
+            }
+            is VGSResponse.ErrorResponse -> responseView.text =
+                "CODE: ${response.errorCode} \n\n ${response.localizeMessage}"
+        }
     }
 }
