d779400076ec0f5d693415728dc964cf8ee82fb0
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-02-11 13:03:58 +0200

    Implement basic UI using native views

--- app/src/main/java/com/vgscollect/androiddemo/NativeControlsActivity.kt
+++ app/src/main/java/com/vgscollect/androiddemo/NativeControlsActivity.kt
@@ -1,27 +1,95 @@
 package com.vgscollect.androiddemo
 
+import android.app.Activity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.InputType
 import android.view.View
-import android.widget.EditText
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_default_components.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NativeControlsActivity : AppCompatActivity() {
+
+    private val dateFormatter = SimpleDateFormat("MM/yy", Locale.getDefault())
+
+    private val inputViews: List<TextInputEditText> by lazy {
+        listOf(etCardNumber, etCardHolderName, etDate, etCVC)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_default_components)
+        initViews()
+        initListeners()
     }
 
-    fun addViewProgrammatically(v: View) {
-        val field = EditText(this)
-        field.setHint("Surname")
-        field.setText("Galt")
-
-        container.removeAllViews()
-        container.addView(field)
-
-        field.requestFocus()
+    private fun initViews() {
+        etDate?.inputType = InputType.TYPE_NULL
     }
 
+    private fun initListeners() {
+        etCardNumber?.doOnTextChanged { text, _, _, _ ->
+            if (text?.trim()?.length == MAX_CARD_NUMBER_LENGTH) {
+                etCardHolderName?.requestFocus()
+            }
+        }
+        etDate?.doOnTextChanged { _, _, _, _ ->
+            etCVC?.requestFocus()
+        }
+        etDate?.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                view.hideKeyboard()
+                showDateDialog()
+            }
+        }
+        mbAddCard?.setOnClickListener {
+            if (!inputViews.any { it.text.isNullOrEmpty() }) {
+                Toast.makeText(this, "Card added!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showDateDialog() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            MaterialDatePicker.Builder.datePicker()
+                .setSelection(currentTimeMillis)
+                .setCalendarConstraints(
+                    CalendarConstraints.Builder().setStart(currentTimeMillis).build()
+                )
+                .build().also {
+                    it.addOnCancelListener {
+                        etDate?.clearFocus()
+                    }
+                    it.addOnNegativeButtonClickListener {
+                        etDate?.clearFocus()
+                    }
+                    it.addOnPositiveButtonClickListener { time ->
+                        etDate?.setText(dateFormatter.format(Date(time)))
+                    }
+                }
+                .show(supportFragmentManager, null)
+        }, 150)
+    }
+
+    companion object {
+
+        private const val MAX_CARD_NUMBER_LENGTH = 16
+    }
+}
+
+val currentTimeMillis: Long
+    get() = System.currentTimeMillis()
+
+fun View.hideKeyboard() {
+    val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as? InputMethodManager
+    imm?.hideSoftInputFromWindow(windowToken, 0)
 }
