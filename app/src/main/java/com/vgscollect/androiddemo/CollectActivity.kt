e0964aea84d57bd89e4e2469b5dc39a21dad01df
Dmytro <dmitriy.kosolapenko@vgs.io>
2021-03-03 20:05:40 +0200

    add Collect screen

--- app/src/main/java/com/vgscollect/androiddemo/CollectActivity.kt
+++ app/src/main/java/com/vgscollect/androiddemo/CollectActivity.kt
@@ -1,27 +1,58 @@
 package com.vgscollect.androiddemo
 
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.verygoodsecurity.vgscollect.widget.PersonNameEditText
+import com.verygoodsecurity.vgscollect.core.Environment
+import com.verygoodsecurity.vgscollect.core.HTTPMethod
+import com.verygoodsecurity.vgscollect.core.VGSCollect
+import com.verygoodsecurity.vgscollect.core.VgsCollectResponseListener
+import com.verygoodsecurity.vgscollect.core.model.network.VGSRequest
+import com.verygoodsecurity.vgscollect.core.model.network.VGSResponse
 import kotlinx.android.synthetic.main.activity_collect_components.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class CollectActivity: AppCompatActivity() {
+
+    private val vgsForm: VGSCollect by lazy {
+        VGSCollect.Builder(this, "tntpszqgikn")
+            .setEnvironment(Environment.SANDBOX)
+            .create()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collect_components)
+
+        mbAddCard?.setOnClickListener { submitData() }
+
+        setupCollect()
     }
 
-    fun addViewProgrammatically(v: View) {
-        val field = PersonNameEditText(this)
-        field.setHint("Surname")
-        field.setText("Galt")
+    private fun submitData() {
+        val request: VGSRequest = VGSRequest.VGSRequestBuilder()
+            .setMethod(HTTPMethod.POST)
+            .setPath("/post")
+            .build()
 
-        container.removeAllViews()
-        container.addView(field)
-
-        field.requestFocus()
+        vgsForm.asyncSubmit(request)
     }
 
+    private fun setupCollect() {
+        vgsForm.bindView(etCardNumber)
+        vgsForm.bindView(etCardHolderName)
+        vgsForm.bindView(etDate)
+        vgsForm.bindView(etCVC)
+
+        vgsForm.addOnResponseListeners(object : VgsCollectResponseListener {
+            override fun onResponse(response: VGSResponse?) {
+                when(response?.code) {
+                    null -> return
+                    200 -> Toast.makeText(this@CollectActivity, "Card added!", Toast.LENGTH_SHORT).show()
+                    else -> Toast.makeText(this@CollectActivity, " $response", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        })
+    }
 }
