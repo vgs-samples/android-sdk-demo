5e2d2819d3f8424d0a8f2259200f1aca498b350a
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-03-24 16:16:55 +0200

    Add cname sample

--- show/src/main/java/com/vgsshow/androiddemo/samples/cname/CnameActivity.kt
+++ show/src/main/java/com/vgsshow/androiddemo/samples/cname/CnameActivity.kt
@@ -1,4 +1,60 @@
 package com.vgsshow.androiddemo.samples.cname
 
-class CnameActivity {
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.verygoodsecurity.vgsshow.VGSShow
+import com.verygoodsecurity.vgsshow.core.listener.VGSOnResponseListener
+import com.verygoodsecurity.vgsshow.core.network.client.VGSHttpMethod
+import com.verygoodsecurity.vgsshow.core.network.model.VGSRequest
+import com.verygoodsecurity.vgsshow.core.network.model.VGSResponse
+import com.verygoodsecurity.vgsshow.widget.VGSTextView
+import com.vgsshow.androiddemo.BuildConfig
+import com.vgsshow.androiddemo.R
+import kotlinx.android.synthetic.main.activity_basic_usage.*
+
+class CnameActivity: AppCompatActivity() {
+
+    /**
+     * Configure custom host name in <a href="https://dashboard.verygoodsecurity.com/">VGS dashboard</a> before using it.
+     */
+    private val vgsShow = VGSShow.Builder(this, BuildConfig.VAULT_ID)
+        .setHostname("<HOST_NAME>")
+        .build()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic_usage)
+
+        // Setup view
+        val tvCardNumber = VGSTextView(this)
+        tvCardNumber.setContentPath("<CONTENT_PATH>")
+        tvCardNumber.setHint("Fetching card number...")
+        tvCardNumber.setHintTextColor(ContextCompat.getColor(this, android.R.color.black))
+        rootView.addView(tvCardNumber)
+
+        // Subscribe view
+        vgsShow.subscribe(tvCardNumber)
+
+        // Handle response
+        vgsShow.addOnResponseListener(object : VGSOnResponseListener {
+
+            override fun onResponse(response: VGSResponse) {
+                Log.d(this@CnameActivity::class.java.simpleName, response.toString())
+            }
+        })
+
+        // Make request
+        vgsShow.requestAsync(
+            VGSRequest.Builder("<PATH>", VGSHttpMethod.POST)
+                .body(mapOf(/** <PAYLOAD> */))
+                .build()
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        vgsShow.onDestroy()
+    }
 }
