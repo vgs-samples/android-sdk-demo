02454a2312c3d7c82d14b48996db46d5070dae98
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-04-21 18:26:56 +0300

    Implement example

--- collect/src/main/java/com/vgscollect/androiddemo/samples/compare/CompareContentActivity.kt
+++ collect/src/main/java/com/vgscollect/androiddemo/samples/compare/CompareContentActivity.kt
@@ -2,8 +2,11 @@ package com.vgscollect.androiddemo.samples.compare
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.verygoodsecurity.vgscollect.core.HTTPMethod
 import com.verygoodsecurity.vgscollect.core.VGSCollect
+import com.verygoodsecurity.vgscollect.view.InputFieldView
 import com.vgscollect.androiddemo.R
+import kotlinx.android.synthetic.main.activity_compare_content.*
 
 class CompareContentActivity : AppCompatActivity() {
 
@@ -13,7 +16,23 @@ class CompareContentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compare_content)
 
-        // Initialize VGSCollect
         vgsForm = VGSCollect(this, "<vault_id>", "<environment>")
+
+        vgsForm.bindView(vgsEtPassword) // You should bind only view that will be send to beck-end
+
+        vgsEtVerifyPassword?.addOnTextChangeListener(object : InputFieldView.OnTextChangedListener {
+
+            override fun onTextChange(view: InputFieldView, isEmpty: Boolean) {
+                vgsTilVerifyPassword.setError(null)
+            }
+        })
+
+        btnSubmit?.setOnClickListener {
+            if (vgsEtPassword.isContentEquals(vgsEtVerifyPassword)) {
+                vgsForm.asyncSubmit("<path>", HTTPMethod.POST)
+            } else {
+                vgsTilVerifyPassword.setError("Password doesn't match!")
+            }
+        }
     }
 }
