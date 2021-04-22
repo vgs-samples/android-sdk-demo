8bc087d7da54723b45fa86132bef4ccffd1f6366
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-04-22 18:34:47 +0300

    Add cname sample

---
+++ collect/src/main/java/com/vgscollect/androiddemo/samples/cname/CnameActivity.kt
@@ -0,0 +1,61 @@
+package com.vgscollect.androiddemo.samples.cname
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.verygoodsecurity.vgscollect.core.HTTPMethod
+import com.verygoodsecurity.vgscollect.core.VGSCollect
+import com.verygoodsecurity.vgscollect.core.VgsCollectResponseListener
+import com.verygoodsecurity.vgscollect.core.model.network.VGSRequest
+import com.verygoodsecurity.vgscollect.core.model.network.VGSResponse
+import com.verygoodsecurity.vgscollect.widget.VGSCardNumberEditText
+import com.vgscollect.androiddemo.R
+import kotlinx.android.synthetic.main.activity_layout.*
+
+class CnameActivity : AppCompatActivity() {
+
+    /**
+     * Configure custom host name in <a href="https://dashboard.verygoodsecurity.com/">VGS dashboard</a> before using it.
+     */
+    private val vgsCollect: VGSCollect by lazy {
+        VGSCollect.Builder(this, "<VAULT_ID>")
+            .setHostname("<HOST_NAME>") // Set custom hostname, for example: https://example.com
+            .create()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout)
+
+        // Setup view
+        val vgsEtCardNumber = VGSCardNumberEditText(this)
+        vgsEtCardNumber.setFieldName("<FIELD_NAME>")
+        vgsEtCardNumber.setHint("Card number")
+        vgsEtCardNumber.setDivider('-')
+        rootView.addView(vgsEtCardNumber)
+
+        // Subscribe view
+        vgsCollect.bindView(vgsEtCardNumber)
+
+        // Handle response
+        vgsCollect.addOnResponseListeners(object : VgsCollectResponseListener {
+
+            override fun onResponse(response: VGSResponse?) {
+                Log.d(this@CnameActivity::class.java.simpleName, response.toString())
+            }
+        })
+
+        // Make request
+        vgsCollect.asyncSubmit(
+            VGSRequest.VGSRequestBuilder()
+                .setPath("<PATH>")
+                .setMethod(HTTPMethod.POST)
+                .build()
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        vgsCollect.onDestroy()
+    }
+}
