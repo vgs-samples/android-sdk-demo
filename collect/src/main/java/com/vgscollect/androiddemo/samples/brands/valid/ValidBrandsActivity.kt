8f4fb2c359a103b93a91619b578e727b450278a2
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-04-23 10:43:37 +0300

    Add valid card brands sample

---
+++ collect/src/main/java/com/vgscollect/androiddemo/samples/brands/valid/ValidBrandsActivity.kt
@@ -0,0 +1,52 @@
+package com.vgscollect.androiddemo.samples.brands.valid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.verygoodsecurity.vgscollect.core.VGSCollect
+import com.verygoodsecurity.vgscollect.util.extension.toCardBrand
+import com.verygoodsecurity.vgscollect.view.card.BrandParams
+import com.verygoodsecurity.vgscollect.view.card.CardBrand
+import com.verygoodsecurity.vgscollect.view.card.CardType
+import com.verygoodsecurity.vgscollect.view.card.validation.payment.ChecksumAlgorithm
+import com.verygoodsecurity.vgscollect.widget.VGSCardNumberEditText
+import com.vgscollect.androiddemo.R
+import kotlinx.android.synthetic.main.activity_layout.*
+
+class ValidBrandsActivity : AppCompatActivity() {
+
+    private val vgsCollect: VGSCollect by lazy {
+        VGSCollect.Builder(this, "<VAULT_ID>").create()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout)
+
+        // Setup view
+        val vgsEtCardNumber = VGSCardNumberEditText(this)
+        vgsEtCardNumber.setFieldName("<FIELD_NAME>")
+        vgsEtCardNumber.setHint("Card number")
+        vgsEtCardNumber.setDivider('-')
+        rootView.addView(vgsEtCardNumber)
+
+        // Subscribe view
+        vgsCollect.bindView(vgsEtCardNumber)
+
+        // Create custom brand
+        val brandParams = BrandParams(
+            "## #### ## #### #### ###",
+            ChecksumAlgorithm.LUHN,
+            arrayOf(19),
+            arrayOf(3)
+        )
+        val cardBrand = CardBrand(
+            "^99",
+            "Custom Brand",
+            R.drawable.ic_visa_dark,
+            brandParams
+        )
+
+        // Set valid card brands
+        vgsEtCardNumber.setValidCardBrands(cardBrand, CardType.VISA.toCardBrand())
+    }
+}
