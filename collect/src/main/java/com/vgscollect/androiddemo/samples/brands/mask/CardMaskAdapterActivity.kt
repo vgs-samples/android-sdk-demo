20c6a0683a35fc1cff2fc9837bcb7f515cea8c44
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-04-26 13:19:18 +0300

    Add card mask sample

---
+++ collect/src/main/java/com/vgscollect/androiddemo/samples/brands/mask/CardMaskAdapterActivity.kt
@@ -0,0 +1,52 @@
+package com.vgscollect.androiddemo.samples.brands.mask
+
+import android.os.Bundle
+import android.view.Gravity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.setPadding
+import com.verygoodsecurity.vgscollect.core.VGSCollect
+import com.verygoodsecurity.vgscollect.view.card.CardType
+import com.verygoodsecurity.vgscollect.view.card.formatter.CardMaskAdapter
+import com.verygoodsecurity.vgscollect.widget.VGSCardNumberEditText
+import com.vgscollect.androiddemo.R
+import kotlinx.android.synthetic.main.activity_layout.*
+
+class CardMaskAdapterActivity : AppCompatActivity() {
+
+    private lateinit var vgsCollect: VGSCollect
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout)
+
+        // Init collect
+        vgsCollect = VGSCollect(this, "<vault_id>", "<environment>")
+
+        // Setup view
+        val vgsEtCardNumber = VGSCardNumberEditText(this).apply {
+            setFieldName("<field_name>")
+            setHint("Card number")
+            setDivider('-')
+            setCardBrandIconGravity(Gravity.END)
+            setPadding(resources.getDimensionPixelSize(R.dimen.padding))
+        }
+        rootView.addView(vgsEtCardNumber)
+
+        // Subscribe view
+        vgsCollect.bindView(vgsEtCardNumber)
+
+        // Add card mask adapter and override VISA mask
+        vgsEtCardNumber.setCardMaskAdapter(object : CardMaskAdapter() {
+
+            override fun getMask(
+                cardType: CardType,
+                name: String,
+                bin: String,
+                mask: String
+            ): String = when (cardType) {
+                CardType.VISA_ELECTRON -> "###### ###### ####"
+                else -> super.getMask(cardType, name, bin, mask)
+            }
+        })
+    }
+}
---
+++ collect/src/main/java/com/vgscollect/androiddemo/samples/brands/mask/README.md
@@ -0,0 +1,12 @@
+# Card mask adapter
+
+This sample of Activity demonstrates how to use card mask adapter.
+
+## Prerequisites
+
+- VGS Collect SDK v.1.1.14
+- Android SDK 19
+
+## Support
+
+If you've found an error in this sample, please submit an issue or write on email: support@verygoodsecurity.com.
